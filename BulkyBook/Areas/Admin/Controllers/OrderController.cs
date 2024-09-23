673078c5833c749265ba7b2f898c9aa0bf810db0
sara-magdi <sara.magdy1996@icloud.com>
2024-09-23 12:06:24 +0200

    edit

---
+++ BulkyBook/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using Bulk.DataAccess.Repository;
+using Bulk.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region call Api
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+        #endregion
+
+    }
+}
